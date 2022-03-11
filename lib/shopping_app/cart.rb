dead7a42422b4a4261a9a505e2c101a4a29ccf63
alexis <alexis@MacBook-Pro.local>
2022-03-11 12:42:43 +0200

    shop task

---
+++ lib/shopping_app/cart.rb
@@ -0,0 +1,51 @@
+require_relative "item_manager"
+require_relative "ownable"
+require_relative "item"
+require_relative "wallet"
+require_relative "seller"
+
+class Cart
+  include ItemManager
+  include Ownable
+
+  def initialize(owner)
+    self.owner = owner
+    @items = []
+  end
+
+  def items
+    # Cartにとってのitemsは自身の@itemsとしたいため、ItemManagerのitemsメソッドをオーバーライドします。
+    # CartインスタンスがItemインスタンスを持つときは、オーナー権限の移譲をさせることなく、自身の@itemsに格納(Cart#add)するだけだからです。
+    @items
+  end
+
+  def add(item)
+    @items << item
+  end
+
+  def total_amount
+    @items.sum(&:price)
+  end
+
+  def check_out
+    # binding.irb
+    return if owner.wallet.balance < total_amount
+    items.each do |item| # additional
+      price = self.owner.wallet.withdraw(item.price)
+      item.owner.wallet.deposit(price)
+      item.owner = self.owner
+      # binding.irb
+    end
+    items.clear
+    # item.owner.wallet.balacne += self.owner.wallet.withdraw(total_amount)
+  # ## 要件
+  #   - カートの中身（Cart#items）のすべてのアイテムの購入金額が、カートのオーナーのウォレットからアイテムのオーナーのウォレットに移されること。
+  #   - カートの中身（Cart#items）のすべてのアイテムのオーナー権限が、カートのオーナーに移されること。
+  #   - カートの中身（Cart#items）が空になること。
+  # ## ヒント
+  #   - カートのオーナーのウォレット ==> self.owner.wallet
+  #   - アイテムのオーナーのウォレット ==> item.owner.wallet
+  #   - お金が移されるということ ==> (？)のウォレットからその分を引き出して、(？)のウォレットにその分を入金するということ
+  #   - アイテムのオーナー権限がカートのオーナーに移されること ==> オーナーの書き換え(item.owner = ?)
+  end
+end
---
+++ lib/shopping_app/wallet.rb
@@ -0,0 +1,19 @@
+require_relative "ownable.rb"
+class Wallet
+  attr_reader :balance
+  include Ownable
+  def initialize(owner)
+    self.owner = owner
+    @balance = 0
+  end
+  def deposit(amount)
+    @balance += amount.to_i
+  end
+
+  def withdraw(amount)
+    return unless @balance >= amount
+    @balance -= amount.to_i
+    amount
+  end
+
+end
